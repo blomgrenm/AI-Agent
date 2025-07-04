fbf67b6af912479696687b9a9a78751f2254f938
blomgrenm <martin_blomgren@hotmail.com>
2025-07-04 16:26:37 +0200

    some tweaks to get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,13 +1,20 @@
 import os
 
+
+
 def get_files_info(working_directory, directory=None):
-    if not os.path.abspath(directory).startswith(os.path.abspath(working_directory)):
-        print(os.path.abspath(directory))
-        print(os.path.abspath(working_directory))
+    directory_path = os.path.abspath(os.path.join(working_directory,directory))
+    working_directory_path = os.path.abspath(working_directory)
+    # if not os.path.abspath(directory).startswith(os.path.abspath(working_directory)):
+    if not directory_path.startswith(working_directory_path):
+        # print(os.path.abspath(directory))
+        # print(os.path.abspath(working_directory))
         return (f'Error: cannot list "{directory}" as it is outside the permitted working directory')
     if not os.path.isdir(directory):
-        print("Test2")
+        # print("Test2")
         return (f'Error: "{directory}" is not a directory')
+    
+    return_list = []
     
     for i in os.listdir(directory):
 
@@ -20,7 +27,10 @@ def get_files_info(working_directory, directory=None):
         except Exception as e:
             return f"Error: {str(e)}"
         
-        print(f"{i}: file_size={size}, is_dir={is_dir}")
+        # print(f"{i}: file_size={size}, is_dir={is_dir}")
+        return_list.append(i+": file_size="+str(size)+", is_dir="+str(is_dir))
+
+    return_string = "\n".join(return_list)
 
 get_files_info("calculator","pkg")
     
