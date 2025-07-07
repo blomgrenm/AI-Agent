b6b1be0f521282493e5bef300b445658e8255aa5
blomgrenm <martin_blomgren@hotmail.com>
2025-07-07 13:47:10 +0200

    get_files_info completed

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,34 +3,35 @@ import os
 
 
 def get_files_info(working_directory, directory=None):
-    directory_path = os.path.abspath(os.path.join(working_directory,directory))
+    directory_path = os.path.abspath(os.path.join(working_directory,directory or '.'))
     working_directory_path = os.path.abspath(working_directory)
     # if not os.path.abspath(directory).startswith(os.path.abspath(working_directory)):
     if not directory_path.startswith(working_directory_path):
-        # print(os.path.abspath(directory))
-        # print(os.path.abspath(working_directory))
         return (f'Error: cannot list "{directory}" as it is outside the permitted working directory')
-    if not os.path.isdir(directory):
+    if not os.path.isdir(directory_path):
         # print("Test2")
         return (f'Error: "{directory}" is not a directory')
     
     return_list = []
     
-    for i in os.listdir(directory):
+    for i in os.listdir(directory_path):
 
+        item_path = os.path.join(directory_path, i)
         try:
-            size = os.path.getsize(i)
+            size = os.path.getsize(item_path)
         except Exception as e:
             return f"Error: {str(e)}"
         try:
-            is_dir = os.path.isdir(i)
+            is_dir = os.path.isdir(item_path)
         except Exception as e:
             return f"Error: {str(e)}"
         
         # print(f"{i}: file_size={size}, is_dir={is_dir}")
-        return_list.append(i+": file_size="+str(size)+", is_dir="+str(is_dir))
+        return_list.append("- "+i+": file_size="+str(size)+", is_dir="+str(is_dir))
 
     return_string = "\n".join(return_list)
 
-get_files_info("calculator","pkg")
+    return return_string
+
+# get_files_info("calculator","pkg")
     
--- tests.py
+++ tests.py
@@ -2,5 +2,5 @@ from functions.get_files_info import get_files_info
 
 print(get_files_info("calculator","."))
 print(get_files_info("calculator","pkg"))
-get_files_info("calculator","/bin")
-get_files_info("calculator","../")
+print(get_files_info("calculator","/bin"))
+print(get_files_info("calculator","../"))
